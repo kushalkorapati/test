e7d50ccaf4dc75a63dfe49f646841698c6df9bca
kushalkorapati <kushalkorapati6@gmail.com>
2023-12-13 23:28:38 +0530

    my git

---
+++ shellscript/installgit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "make sure that your are a root user"
+    exit 1
+else
+    echo "yes you are a root user installing mysql"
+fi
+
+yum install git -y
+
+if [ $! -ne 0 ]
+then
+    echo "you get the error "
+    extit1
+else
+    echo "you are sucessfully installing the package"
+fi
+
