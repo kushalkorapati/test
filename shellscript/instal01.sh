048fe18f98fe5daa1cb2e1280db200e2f69e60bb
kushalkorapati <kushalkorapati6@gmail.com>
2023-12-13 23:04:42 +0530

    my sql

--- shellscript/instal01.sh
+++ shellscript/instal01.sh
@@ -5,9 +5,18 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "make sure that your are a root user"
+    exit 1
 else
     echo "yes you are a root user installing mysql"
 fi
 
 yum install mysql -y
+
+if [ $! -ne 0 ]
+then
+    echo "you get the error "
+    extit1
+else
+    echo "you are sucessfully installing the package"
+fi
 
