3ddb4c6838b2321d4b4e7c06f450755241461ad6
kushalkorapati <kushalkorapati6@gmail.com>
2023-12-14 22:22:57 +0530

    installing packages

--- shellscript/installpackage01.sh
+++ shellscript/installpackage01.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ID=$(id -u)
-R="\e[0;31m"
-G="\e[0;32m"
+R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 if [ ID -ne 0 ]
 then 
-    echo "$R ERROR : You are not a root user $N"
+    echo -e "$R ERROR : You are not a root user $N"
     exit 1
 else
     echo "$G you are a root user"
