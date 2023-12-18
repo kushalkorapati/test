71f6cd885083cd7138049c5eb4e209cae2cf3522
kushalkorapati <kushalkorapati6@gmail.com>
2023-12-18 18:31:41 +0530

    1st assigement

--- assigements/rows-columns/rows-columns.sh
+++ assigements/rows-columns/rows-columns.sh
@@ -1,4 +1,11 @@
  #!/bin/bash
 
- File="rows-columns/file.txt/"
-  echo "$File"
+ #File="/rows-columns/file.txt/"
+#echo "$File"
+
+input_file="$1"
+num_cols=$(head -1 "$input_file" | wc -w)
+for i in $(seq 1 "$num_cols"); 
+do
+echo $(cut -d ' ' -f "$i" "$input_file")
+done
