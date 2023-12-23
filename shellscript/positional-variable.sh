ccf15283843b5c37c224ef25a0a90f426e1d4b7d
kushalkorapati <kushalkorapati6@gmail.com>
2023-12-23 17:14:45 +0530

    my second commit

--- shellscript/positional-variable.sh
+++ shellscript/positional-variable.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-user="$1"
-rollno="$2"
-class="$3"
+# user="$1"
+# rollno="$2"
+# class="$3"
 
 
-echo "my name is $user and my rollno is $2 and my class is $class"
-echo "$0" 
+# echo "my name is $user and my rollno is $2 and my class is $class"
+# echo "$0" # here $0 will show the name of the script
+
+user="$@"
+ echo "hi my details  is $user and my name $1 and my roll is $2 and my $3"
